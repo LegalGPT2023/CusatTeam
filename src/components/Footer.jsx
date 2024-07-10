2b2a83ef1346d3406351e8e65ce43b385b5847fb
SHUBHAM KUMAR <shubhamkumar@SHUBHAMs-MacBook-Air.local>
2024-07-10 10:13:27 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import styles from "../style";
 import { logo } from "../assets";
 import { footerLinks, socialMedia } from "../constants";
+import { FaRegCopyright } from "react-icons/fa";
 
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
@@ -12,7 +13,8 @@ const Footer = () => (
           className="w-[266px] h-[72.14px] object-contain"
         />
         <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
-          <span className="text-gradient font-bold">LegalGPT</span>{" - "}
+          <span className="text-gradient font-bold">LegalGPT</span>
+          {" - "}
           An innovative solution to better the legal system.
         </p>
       </div>
@@ -44,8 +46,9 @@ const Footer = () => (
     </div>
 
     <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
-      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
-        Thank you for visiting our website. 😊
+      <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white inline-flex items-center space-x-1">
+        <FaRegCopyright />
+        <span>2024 CUSAT Team. All rights reserved.</span>
       </p>
 
       <div className="flex flex-row md:mt-0 mt-6">
